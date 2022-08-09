4be8d48f4933fbae1927bb6afe8a39ed8c5958e9
Jeff Lupton <jeffreylupton123@gmail.com>
2022-08-09 12:24:13 -0700

    dynamic fetching/loading of NPM modules. Fix URL discrepencies

--- src/plugins/package-path-plugin.tsx
+++ src/plugins/package-path-plugin.tsx
@@ -20,18 +20,19 @@ export const unpkgPathPlugin = () => {
           return { path: args.path, namespace: "a" };
         }
 
-        if (args.path.includes("./") || args.path.includes("../"))
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
             namespace: "a",
-            // new URL creates a new URL with the following args new URL(addOnURL, baseURL) it is important to include the + '/' at the end of args.importer in order to get the correct functionality
-            path: new URL(args.path, args.importer + "/").href,
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
           };
-        //else if (args.path === "tiny-test-pkg") {
-        //   return {
-        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-        //     namespace: "a",
-        //   };
-        // }
+        }
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
       });
       //onLoad used to actually load a file from the file_system
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -41,15 +42,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('medium-test-pkg')
+              const message = require('react')
               console.log(message);
             `,
           };
         }
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
         return {
           loader: "jsx",
           contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
       });
     },
